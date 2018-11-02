8a39e3f83a9bb2445a0a138f99a7920c65e3c3f9
Atsushi Watanabe <atsushi.w@ieee.org>
2018-11-02 13:05:35 +0900

    Callback EOS and stream error (#1)

--- gstlaunch/gstlaunch.go
+++ gstlaunch/gstlaunch.go
@@ -1,37 +1,46 @@
 package gstlaunch
 
 import (
+	"fmt"
+	"runtime"
 	"unsafe"
 )
 
 // #cgo pkg-config: gobject-2.0 gstreamer-1.0 gstreamer-base-1.0 gstreamer-rtsp-server-1.0 gstreamer-rtsp-1.0
 // #include <stdlib.h>
-// #include <stdio.h>
 // #include <gst/gst.h>
 //
-// typedef void* Mainloop;
+// typedef struct
+// {
+//   GMainLoop *mainloop;
+//   int user_int;
+// } Context;
 //
-// void init()
+// extern void goCbEOS(int);
+// extern void goCbError(int);
+//
+// static void init()
 // {
 //   int argc = 1;
 //   char *exec_name = "rtsp_receiver";
 //   char **argv = &exec_name;
 //   gst_init(&argc, &argv);
 // }
-// gboolean cbMessage(GstBus *bus, GstMessage *msg, gpointer p)
+// static gboolean cbMessage(GstBus *bus, GstMessage *msg, gpointer p)
 // {
-//   GMainLoop *mainloop = (GMainLoop*)p;
+//   Context *ctx = (Context*)p;
 //
 //   if ((GST_MESSAGE_TYPE(msg) & GST_MESSAGE_EOS))
-//     g_main_loop_quit(mainloop);
+//     goCbEOS(ctx->user_int);
 //
 //   if ((GST_MESSAGE_TYPE(msg) & GST_MESSAGE_ERROR))
-//     g_main_loop_quit(mainloop);
+//     goCbError(ctx->user_int);
 //
 //   return TRUE;
 // }
-// Mainloop create(const char *launch)
+// static Context *create(const char *launch, int user_int)
 // {
+//   Context *ctx;
 //   GMainLoop *mainloop;
 //   GstElement *pipeline;
 //   GError *err = NULL;
@@ -46,23 +55,25 @@ import (
 //     g_object_unref(mainloop);
 //     return NULL;
 //   }
+//   ctx = malloc(sizeof(Context));
+//   ctx->mainloop = mainloop;
+//   ctx->user_int = user_int;
+//
 //   bus = gst_element_get_bus(pipeline);
-//   gst_bus_add_watch(bus, cbMessage, mainloop);
+//   gst_bus_add_watch(bus, cbMessage, ctx);
 //   g_object_unref(bus);
 //
 //   gst_element_set_state(pipeline, GST_STATE_PLAYING);
 //
-//   return mainloop;
+//   return ctx;
 // }
-// void mainloopRun(Mainloop p)
+// static void mainloopRun(Context *ctx)
 // {
-//   GMainLoop *mainloop = (GMainLoop*)p;
-//   g_main_loop_run(mainloop);
+//   g_main_loop_run(ctx->mainloop);
 // }
-// void mainloopKill(Mainloop p)
+// static void mainloopKill(Context *ctx)
 // {
-//   GMainLoop *mainloop = (GMainLoop*)p;
-//   g_main_loop_quit(mainloop);
+//   g_main_loop_quit(ctx->mainloop);
 // }
 import "C"
 
@@ -71,26 +82,75 @@ func init() {
 }
 
 type GstLaunch struct {
-	mainloop C.Mainloop
-	quit     chan bool
-	active   bool
+	ctx     *C.Context
+	quit    chan bool
+	active  bool
+	cbEOS   func(*GstLaunch)
+	cbError func(*GstLaunch)
 }
+
+var (
+	cPointerMap          = make(map[int]*GstLaunch)
+	cPointerMapIndex int = 0
+)
 
 func New(launch string) *GstLaunch {
 	c_launch := C.CString(launch)
 	defer C.free(unsafe.Pointer(c_launch))
 
-	mainloop := C.create(c_launch)
-	if mainloop == nil {
+	l := &GstLaunch{quit: make(chan bool, 1), active: false, cbEOS: nil, cbError: nil}
+	cPointerMap[cPointerMapIndex] = l
+
+	ctx := C.create(c_launch, C.int(cPointerMapIndex))
+	if ctx == nil {
 		panic("Failed to parse gst-launch text")
 	}
+	l.ctx = ctx
 
-	return &GstLaunch{mainloop: mainloop, quit: make(chan bool, 1)}
+	cPointerMapIndex++
+
+	runtime.SetFinalizer(l, finalizeGstLaunch)
+
+	return l
+}
+
+func finalizeGstLaunch(s *GstLaunch) {
+	C.free(unsafe.Pointer(s.ctx))
+}
+
+func (s *GstLaunch) RegisterErrorCallback(f func(*GstLaunch)) {
+	s.cbError = f
+}
+
+func (s *GstLaunch) RegisterEOSCallback(f func(*GstLaunch)) {
+	s.cbEOS = f
+}
+
+//export goCbEOS
+func goCbEOS(i C.int) {
+	s, ok := cPointerMap[int(i)]
+	if !ok {
+		panic(fmt.Errorf("Failed to map pointer from cgo func (%d)", int(i)))
+	}
+	if s.cbEOS != nil {
+		s.cbEOS(s)
+	}
+}
+
+//export goCbError
+func goCbError(i C.int) {
+	s, ok := cPointerMap[int(i)]
+	if !ok {
+		panic(fmt.Errorf("Failed to map pointer from cgo func (%d)", int(i)))
+	}
+	if s.cbError != nil {
+		s.cbError(s)
+	}
 }
 
 func (s *GstLaunch) Run() {
 	s.active = true
-	C.mainloopRun(s.mainloop)
+	C.mainloopRun(s.ctx)
 	s.quit <- true
 	s.active = false
 }
@@ -98,9 +158,8 @@ func (s *GstLaunch) Run() {
 func (s *GstLaunch) Wait() {
 	<-s.quit
 }
-
 func (s *GstLaunch) Kill() {
-	C.mainloopKill(s.mainloop)
+	C.mainloopKill(s.ctx)
 }
 
 func (s *GstLaunch) Active() bool {
