27abcc3c9268e96b9656ff0229448901384cc20c
Atsushi Watanabe <atsushi.w@ieee.org>
2025-08-12 14:13:29 +0900

    Fix improper GObjectClass initialization in Element.GetProperty

--- element.go
+++ element.go
@@ -45,9 +45,8 @@ package gst
 // GValue* getProperty(void* element, const char* name)
 // {
 //   g_object_ref(element);
-//   GObjectClass klass;
-//   klass.g_type_class.g_type = G_OBJECT_TYPE(element);
-//   GParamSpec* pspec = g_object_class_find_property(&klass, name);
+//   GObjectClass *klass = G_OBJECT_GET_CLASS(element);
+//   GParamSpec* pspec = g_object_class_find_property(klass, name);
 //   if (pspec == NULL)
 //   {
 //     g_object_unref(element);
